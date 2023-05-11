1af6ce632f57174443d7d3dc5f7f881370d0bcd3
Khaled Saleh <khaled@connou.app>
2023-05-11 10:29:00 +0200

    adding methods to perform delete and put http operations

--- src/main/java/com/khaledsaleh/CustomerController.java
+++ src/main/java/com/khaledsaleh/CustomerController.java
@@ -5,24 +5,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/customers")
 public class CustomerController {
     private final CustomerRepository customerRepository;
 
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
-    @GetMapping("/customers")
+    @GetMapping("")
     public List<Customer> getCustomers(){
         return customerRepository.findAll();
     }
 
-    @PostMapping("/add-customer")
+    @PostMapping("")
     public void addCustomer(@RequestBody NewCustomerRequest request){
         Customer customer = new Customer();
         customer.setName(request.name());
         customer.setAge(request.age());
         customer.setEmail(request.email());
         customerRepository.save(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable("id") Integer id){
+        customerRepository.deleteById(id);
+    }
+    @PutMapping("/{id}")
+    public void updateCustomer(@RequestBody NewCustomerRequest request, @PathVariable("id") Integer id){
+        if (customerRepository.existsById(id)){
+            customerRepository.deleteById(id);
+        }
+        addCustomer(request);
     }
 }
