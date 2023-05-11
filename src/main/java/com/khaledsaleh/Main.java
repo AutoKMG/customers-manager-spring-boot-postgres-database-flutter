c4dd5c14b877d2a8487a31f6d45578bc27fa1336
Khaled Saleh <khaled@connou.app>
2023-05-11 10:08:44 +0200

    removing the @RestController and @RequestMapping to a separate controller

--- src/main/java/com/khaledsaleh/Main.java
+++ src/main/java/com/khaledsaleh/Main.java
@@ -4,12 +4,16 @@ package com.khaledsaleh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
